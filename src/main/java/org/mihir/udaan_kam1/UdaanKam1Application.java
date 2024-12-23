4eac3345500424a829fd3e23b378ce5bda39b873
Vinayak Das <vindas@cisco.com>
2024-12-23 19:51:41 +0530

    ADD: Initial Template

---
+++ src/main/java/org/mihir/udaan_kam1/UdaanKam1Application.java
@@ -0,0 +1,13 @@
+package org.mihir.udaan_kam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdaanKam1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdaanKam1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/mihir/udaan_kam1/UdaanKam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mihir.udaan_kam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdaanKam1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
